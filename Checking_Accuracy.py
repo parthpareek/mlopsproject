fb1af4457eda34306ac0aa7c61ba761d7473f8cd
Parth Pareek <parthpareek86@.com>
2020-05-29 15:56:27 +0530

    first commit

---
+++ Checking_Accuracy.py
@@ -0,0 +1,11 @@
+import os
+
+f=open('/model_files/output.txt')
+content=f.read()
+accuracy=int(content[-47:-45])
+print("Accuracy is ",accuracy)
+if accuracy < 80:
+	print("Accuracy is less than 80 Running Hyper Parameter File")
+	os.system("python3 /model_files/tuning_script.py")
+else:
+	print("Accuracy is Greater Than 80 [Accuracy :- {}]".format(accuracy))
